3559fe8006c76e4f9cffec8bf37f18c3455d62bd
Prathamesh Raut <prathamesh.raut@neoquant.com>
2020-07-07 16:29:07 +0530

    reset form using reset() with formReference

--- forms-td-start/src/app/app.component.ts
+++ forms-td-start/src/app/app.component.ts
@@ -54,5 +54,7 @@ export class AppComponent {
     this.user.secretQuestion = this.signupForm.value.secret;
     this.user.questionAnswer = this.signupForm.value.questionAnswer;
     this.user.gender = this.signupForm.value.gender;
+
+    this.signupForm.reset();
   }
 }
